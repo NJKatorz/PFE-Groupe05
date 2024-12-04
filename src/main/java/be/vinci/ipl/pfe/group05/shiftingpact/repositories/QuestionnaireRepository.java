4dcb674dc8cb85db7609c0f65f851a73d5016999
Sikawie <mariam.miclauri@student.vinci.be>
2024-12-04 10:30:18 +0100

    feat: initialization of frontend module

--- src/main/java/be/vinci/ipl/pfe/group05/shiftingpact/repositories/QuestionnaireRepository.java
+++
@@ -1,8 +0,0 @@
-package be.vinci.ipl.pfe.group05.shiftingpact.repositories;
-
-import be.vinci.ipl.pfe.group05.shiftingpact.models.Questionnaire;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface QuestionnaireRepository extends MongoRepository<Questionnaire, Integer> {
-
-}
