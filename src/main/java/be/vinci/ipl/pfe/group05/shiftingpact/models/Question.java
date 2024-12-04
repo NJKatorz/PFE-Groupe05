463033b3b0ed43210c280f1eaabc425e2980e5c5
dania-ouarrad-vinci <113683984+dania-ouarrad-vinci@users.noreply.github.com>
2024-12-04 16:24:58 +0100

    add : forms creation

--- src/main/java/be/vinci/ipl/pfe/group05/shiftingpact/models/Question.java
+++ src/main/java/be/vinci/ipl/pfe/group05/shiftingpact/models/Question.java
@@ -17,14 +17,13 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Question {
-  @Field("question_id")
   private ObjectId id;
 
   @Field("question_id")
   private int questionId;
-  @NonNull
-  @Field("company_id")
-  private int companyID;
+//  @NonNull
+ // @Field("company_id")
+ // private int companyID;
   @NonNull
   private String question;
   private List<String> choice;
