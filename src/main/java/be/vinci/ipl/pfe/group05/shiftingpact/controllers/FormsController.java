95248ef80b8d1c40f2e3e92b06e1e733175d856e
NJKatorz <nathanesakwa@gmail.com>
2024-12-05 17:44:12 +0100

    fix: commit

--- src/main/java/be/vinci/ipl/pfe/group05/shiftingpact/controllers/FormsController.java
+++ src/main/java/be/vinci/ipl/pfe/group05/shiftingpact/controllers/FormsController.java
@@ -5,11 +5,13 @@ import be.vinci.ipl.pfe.group05.shiftingpact.models.Form;
 import be.vinci.ipl.pfe.group05.shiftingpact.services.FormsService;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class FormsController {
   @Autowired
@@ -23,5 +25,7 @@ public class FormsController {
   public Iterable<Form> getAllForms() {
     return service.getAllForms();
   }
+
+
 
 }
--- webapp/PFE/src/views/AllClientForms.vue
+++ webapp/PFE/src/views/AllClientForms.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-    <h1>Liste des Formulaires</h1>
-    <ul>
-      <li v-for="form in forms" :key="form.id">
-        {{ form.question }} - {{ product.enjeux }}
-      </li>
-    </ul>
-  </div>
-</template>
+      <h1>Liste des Formulaires</h1>
+      <ul>
+        <li v-for="form in forms" :key="form.formId">
+          {{ form.companyId }} - {{ form.total }}
+        </li>
+      </ul>
+    </div>
+  </template>
 
 <script>
 import FormsService from '@/services/FormsService';
@@ -23,7 +23,8 @@ export default {
     async fetchForms() {
       try {
         const response = await FormsService.getAllFormClients();
-        this.products = response.data; // Stocker les données dans la liste
+        console.log('Données récupérées :', response.data); // Ajoute ceci pour vérifier les données
+        this.forms = response.data; // Stocker les données dans la liste
       } catch (error) {
         console.error('Erreur lors de la récupération des formulaires:', error);
       }
