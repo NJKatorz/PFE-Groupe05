656744215c755ccbb19cb214c9189417e6a5a501
Manal <manal.hebbal@student.vinci.be>
2024-12-05 15:08:30 +0100

    add: liaison back front + adaptation reponse/type dans le front

--- src/main/java/be/vinci/ipl/pfe/group05/shiftingpact/controllers/QuestionsController.java
+++ src/main/java/be/vinci/ipl/pfe/group05/shiftingpact/controllers/QuestionsController.java
@@ -4,10 +4,12 @@ package be.vinci.ipl.pfe.group05.shiftingpact.controllers;
 import be.vinci.ipl.pfe.group05.shiftingpact.models.Question;
 import be.vinci.ipl.pfe.group05.shiftingpact.services.QuestionsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class QuestionsController {
   @Autowired
